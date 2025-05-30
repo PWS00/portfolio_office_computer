f5eda320d26506520fd54f8fa9f91ab9373d8d84
Pietro Walter Soriano <79905658+PWS00@users.noreply.github.com>
2025-05-30 18:16:35 +0200

    Update About.tsx

--- src/components/showcase/About.tsx
+++ src/components/showcase/About.tsx
@@ -16,9 +16,9 @@ const About: React.FC<AboutProps> = (props) => {
             <br />
             <div className="text-block">
                 <p>
-                    I'm a software engineer currently working at Vercel! In May
-                    of 2022 I graduated from Rensselaer Polytechnic Institute
-                    with my BS in Computer Science.
+                    I'm a cloud-developer and software engineer currently working at IBM Client Innovation Center! In Dec
+                    of 2024 I graduated from Università degli Studi di Bari
+                    with my Master's Degree in Computer Science. 🎓💻
                 </p>
                 <br />
                 <p>
@@ -27,8 +27,8 @@ const About: React.FC<AboutProps> = (props) => {
                     building it. If you have any questions or comments, feel
                     free to contact me using{' '}
                     <Link to="/contact">this form</Link> or shoot me an email at{' '}
-                    <a href="mailto:henryheffernan@gmail.com">
-                        henryheffernan@gmail.com
+                    <a href="mailto:waltersoriano@libero.it">
+                        waltersoriano@libero.it
                     </a>
                 </p>
             </div>
@@ -37,14 +37,14 @@ const About: React.FC<AboutProps> = (props) => {
                 <h3>About Me</h3>
                 <br />
                 <p>
-                    From a young age, I have had a curiosity about how things
-                    worked. This naturally led me to become absolutely obsessed
-                    with Lego and I fell in love with building things. In
-                    elementary school, I joined the Lego Robotics team at my
-                    local middle school, which was my first real exposure to
-                    programming. In 2008, my family and I moved across the
-                    country from California to New York, where I attended middle
-                    school, high school, and college.
+                    I consider myself a person who is extremely passionate
+                    about what I do and curious! I like to study, experiment and
+                    learn from it. My determination has led me to achieve my
+                    goals over the years, learning how the values of discipline
+                    and passion can lead you anywhere. I am passionate about sports,
+                    which is why I work as a football referee.
+                    In addition, I am very dedicated to marketing/digital communication,
+                    in fact as a hobby I am the Social Media Manager for some agencies.
                 </p>
                 <br />
                 <div className="captioned-image">
@@ -52,7 +52,7 @@ const About: React.FC<AboutProps> = (props) => {
                     <p>
                         <sub>
                             <b>Figure 1:</b> A real photo of me developing this
-                            website :)
+                            website :D
                         </sub>
                     </p>
                 </div>
@@ -60,19 +60,6 @@ const About: React.FC<AboutProps> = (props) => {
                 <p>
                     I started programming more seriously in high school,
                     initially learning how to scrape and interact with websites.
-                    I went on to do a ton of passion projects, many of them with
-                    one of my closest friends,{' '}
-                    <a
-                        rel="noreferrer"
-                        target="_blank"
-                        href="https://www.linkedin.com/in/scott-bass-189a7919b/"
-                    >
-                        Scott Bass
-                    </a>
-                    . We worked on many projects together, including chat bots,
-                    multiple game projects, apps, and more. One of these
-                    projects is viewable on my{' '}
-                    <Link to="/projects/software">Software Projects</Link> page.
                 </p>
                 <br />
                 <p>
@@ -149,8 +136,8 @@ const About: React.FC<AboutProps> = (props) => {
                     them. You can reach me through the{' '}
                     <Link to="/contact">contact page</Link> or shoot me an email
                     at{' '}
-                    <a href="mailto:henryheffernan@gmail.com">
-                        henryheffernan@gmail.com
+                    <a href="mailto:waltersoriano@libero.it">
+                        waltersoriano@libero.it
                     </a>
                 </p>
             </div>
